2de22dd2e8a9c9423d284e4710be8572db493694
Hamdi-Abdelmaksoud <hamdi.wdev@gmail.com>
2023-05-09 15:13:40 +0100

    midfier type publishdat postdate (base de donées)

--- base de données/Requête #2.sql
+++ base de données/Requête #2.sql
@@ -1,13 +1,38 @@
-CREATE TABLE Users(
+CREATE TABLE user(
    id_user INT NOT NULL AUTO_INCREMENT,
    pseudo VARCHAR(20) NOT NULL,
    password VARCHAR(50) NOT NULL,
-   inscriptionDate DATE NOT NULL,
+   inscriptionDate DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
    role VARCHAR(50) NOT NULL,
    email VARCHAR(50),
    PRIMARY KEY(id_user),
    UNIQUE(pseudo)
 );
+
+
+CREATE TABLE Topic(
+   id_topic INT NOT NULL AUTO_INCREMENT,
+   title VARCHAR(20) NOT NULL,
+   actif BOOLEAN NOT NULL,
+   publishDate DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+   category_id INT NOT NULL,
+   user_id INT NOT NULL,
+   PRIMARY KEY(id_topic),
+   FOREIGN KEY(category_id) REFERENCES Category(id_category),
+   FOREIGN KEY(user_id) REFERENCES User(id_user)
+);
+
+CREATE TABLE Post(
+   id_poste INT NOT NULL AUTO_INCREMENT,
+   texte TEXT NOT NULL,
+   postDate DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ,
+   user_id INT NOT NULL,
+  topic_id INT NOT NULL,
+   PRIMARY KEY(id_poste),
+   FOREIGN KEY(user_id) REFERENCES User(id_user),
+   FOREIGN KEY(topic_id) REFERENCES Topic(id_topic)
+);
+
 
 CREATE TABLE Category(
    id_category INT NOT NULL AUTO_INCREMENT,
@@ -15,25 +40,3 @@ CREATE TABLE Category(
    PRIMARY KEY(id_category)
 );
 
-CREATE TABLE Topic(
-   id_topic INT NOT NULL AUTO_INCREMENT,
-   title VARCHAR(20) NOT NULL,
-   actif BOOLEAN NOT NULL,
-   publishDate DATE NOT NULL,
-   id_category INT NOT NULL,
-   id_user INT NOT NULL,
-   PRIMARY KEY(id_topic),
-   FOREIGN KEY(id_category) REFERENCES Category(id_category),
-   FOREIGN KEY(id_user) REFERENCES Users(id_user)
-);
-
-CREATE TABLE Post(
-   id_poste INT NOT NULL AUTO_INCREMENT,
-   texte TEXT NOT NULL,
-   postDate DATE NOT NULL,
-   id_user INT NOT NULL,
-   id_topic INT NOT NULL,
-   PRIMARY KEY(id_poste),
-   FOREIGN KEY(id_user) REFERENCES Users(id_user),
-   FOREIGN KEY(id_topic) REFERENCES Topic(id_topic)
-);
