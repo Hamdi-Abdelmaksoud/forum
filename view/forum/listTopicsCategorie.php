13f0cee4ae2da90f1ba4d693f4136374babc0c26
Hamdi-Abdelmaksoud <hamdi.wdev@gmail.com>
2023-05-10 19:07:36 +0100

    améliorer listtopicscategory

--- view/forum/listTopicsCategorie.php
+++ view/forum/listTopicsCategorie.php
@@ -5,9 +5,14 @@ $category=$result['data']['category'];
 ?>
 <h3><?= $category->getNom() ?></h3>
 <?php
+if($topics ==null)
+{
+    echo "pas de topic dans cette catégorie";
+}
+else{
 foreach ($topics as $topic) { ?>
 <ul>
-    <li><?= $topic->getTitle() ?></li>
+    <li><a href="index.php?ctrl=forum&action=listPostsTopic&id=<?=$topic->getId()?>"><?= $topic->getTitle() ?></li>
     </ul>
 <?php
-}
+}}?>
